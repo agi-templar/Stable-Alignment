9a2995e3883246983ea47eb3893b9aae4f72c973
Ruixin (Ray) Yang <42333187+99starman@users.noreply.github.com>
2023-06-18 09:37:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     author="Ruibo Liu",
     author_email="ruibo.liu.gr@dartmouth.edu",
     license="Apache v2.0",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
@@ -65,12 +65,12 @@ setup(
         # Pick your license as you wish (should match "license" above)
         "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 3.6",
+        # that you indicate whether you support Python 2, Python 3 or both.        
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     keywords="natural language processing language model",
     packages=find_packages(exclude=["test", "data", "test.*"]),
